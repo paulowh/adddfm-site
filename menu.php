39d99227de4d987a282e78079763928efa2d0371
Paulo Henrique <42096755+paulowh@users.noreply.github.com>
2022-06-28 20:29:03 -0300

    adicionando o projeto inicial puramente em Bootstrap

---
+++ menu.php
@@ -0,0 +1,25 @@
+<li><a href="index.php" class="active">Inicio</a></li>
+          <li><a href="about.php">Sobre</a></li>
+          <li><a href="services.php">Serviços</a></li>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="team.php">Time</a></li>
+          <!-- <li><a href="blog.php">Blog</a></li> 
+          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="#">Dropdown 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Dropdown 2</a></li>
+              <li><a href="#">Dropdown 3</a></li>
+              <li><a href="#">Dropdown 4</a></li>
+            </ul>
+          </li>-->
+          <li><a href="contact.php">Contato</a></li>
+        </ul>
