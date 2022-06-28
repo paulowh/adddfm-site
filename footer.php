f56b6f3a3b1e3a4c43e63b9ac31ef1a858eedc8e
Paulo Henrique <42096755+paulowh@users.noreply.github.com>
2022-06-28 20:54:21 -0300

    alteração do menu e do rodapé para php

--- footer.php
+++ footer.php
@@ -1,68 +1,69 @@
 <div class="footer-content">
-      <div class="container">
+    <div class="container">
         <div class="row gy-4">
-          <div class="col-lg-5 col-md-12 footer-info">
-            <a href="index.html" class="logo d-flex align-items-center">
-              <span>ADDDFM</span>
-            </a>
-            <p>Associação Defensora Dos Direitos Dos Deficientes Físicos E Da Mulher.</p>
-            <div class="social-links d-flex  mt-3">
-              <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> -->
-              <a href="https://www.facebook.com/ADDDFM/" ctarget="_blanc" lass="facebook"><i class="bi bi-facebook"></i></a>
-              <a href="https://www.instagram.com/adddfm/" target="_blanc" class="instagram"><i class="bi bi-instagram"></i></a>
-              <a href="https://www.linkedin.com/company/adddfm/" target="_blanc"  class="linkedin"><i class="bi bi-linkedin"></i></a>
+            <div class="col-lg-5 col-md-12 footer-info ">
+                <a href="index.html" class="logo d-flex align-items-center">
+                    <span>ADDDFM</span>
+                </a>
+                <p>Associação Defensora Dos Direitos Dos Deficientes Físicos E Da Mulher.</p>
+                <div class="social-links d-flex  mt-3 ">
+                    <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> -->
+                    <a href="https://www.facebook.com/ADDDFM/" ctarget="_blanc" lass="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="https://www.instagram.com/adddfm/" target="_blanc" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="https://www.linkedin.com/company/adddfm/" target="_blanc" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                </div>
             </div>
-          </div>
 
-          <div class="col-lg-2 col-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bi bi-dash"></i> <a href="#">Home</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">About us</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Services</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Links Uteis</h4>
+                <ul>
+                    <li><i class="bi bi-dash"></i> <a href="index.php">Inicio</a></li>
+                    <li><i class="bi bi-dash"></i> <a href="about.php">Sobre</a></li>
+                    <li><i class="bi bi-dash"></i> <a href="services.php">Serviços</a></li>
+                    <li><i class="bi bi-dash"></i> <a href="#">Termos de serviços</a></li>
+                    
 
-          <div class="col-lg-2 col-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bi bi-dash"></i> <a href="#">Web Design</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Web Development</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Product Management</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Marketing</a></li>
-              <li><i class="bi bi-dash"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
+                </ul>
+            </div>
 
-          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-            <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Outros</h4>
+                <ul>
+                    <li><i class="bi bi-dash"></i> <a href="#">Blog</a></li>
+                    <li><i class="bi bi-dash"></i> <a href="#">Portfolio</a></li>
+                    <li><i class="bi bi-dash"></i> <a href="#">Quero me voluntariar</a></li>
+                    <li><i class="bi bi-dash"></i> <a href="#">Politica de privacidade</a></li>
 
-          </div>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                <h4>Contato</h4>
+                <p>
+                    Rua Corolampio Trilas <br>
+                    Cidade Tiradentes, 08471-260<br>
+                    São Paulo <br>
+                    <strong>Telefone:</strong> <a>(11)2964-9294</a><br>
+                    <strong>Email:</strong> contato@adddfm.org.br<br>
+                </p>
+
+            </div>
 
         </div>
-      </div>
     </div>
+</div>
 
-    <div class="footer-legal">
-      <div class="container">
+<div class="footer-legal">
+    <div class="container">
         <div class="copyright">
-          &copy; Copyright <strong><span>Nova</span></strong>. All Rights Reserved
+            <strong><span>Adddfm</span></strong> ONG Beneficente
         </div>
         <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nova-bootstrap-business-template/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nova-bootstrap-business-template/ -->
+            desenvolvido por <a href="https://paulowh.github.io">Paulo "Who" Santos</a>
         </div>
-      </div>
     </div>
+</div>
