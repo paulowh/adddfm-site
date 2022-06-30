b36b85671dc79f13b1f5f3cdd289fff74a42254d
Paulo <ph744915@gmail.com>
2022-06-30 15:19:43 -0300

    correção de envio de email

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,52 @@
 <?php
-    //Variáveis
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $data_envio = date('d/m/Y');
-    $hora_envio = date('H:i:s');
+require_once('../assets/PHPMailer/src/PHPMailer.php');
+require_once('../assets/PHPMailer/src/SMTP.php');
+require_once('../assets/PHPMailer/src/Exception.php');
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    //Compo E-mail
-    $arquivo = "
-        <html>
-        <p><b>Nome: </b>$name</p>
-        <p><b>E-mail: </b>$email</p>
-        <p><b>Mensagem: </b>$subject</p>
-        <p><b>Mensagem: </b>$message</p>
-        <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-        </html>
-    ";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-    //Emails para quem será enviado o formulário
-    $destino = "paulo.whsantos@hotmail.com";
-    $assunto = "Contato pelo Site";
+$arquivo = "
+<html>
+  <p><strong>Nome: </strong>$name</p>
+  <p><strong>E-mail: </strong>$email</p>
+  <p><strong>Mensagem: </strong>$message</p>
+  
+</html>
+";
+$mail = new PHPMailer;
+ 
+try {
+	// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->isSMTP();
+	$mail->Host = '	mail.adddfm.org.br';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'contato@adddfm.org.br';
+	$mail->Password = '201919*adddfm';
+	$mail->Port = 587;
+ 
+	$mail->setFrom($email);
+	$mail->addAddress('contato@adddfm.org.br');
+	// $mail->addAddress('endereco2@provedor.com.br');
+ 
+	$mail->isHTML(true);
+	$mail->Subject = '[SISTEMA SITE] ' . $subject;
+	$mail->Body = $arquivo;
+	// $mail->AltBody = 'Chegou o email teste do Paulo 587';
+ 
+	if($mail->send()) {
+		echo 'Sua mensagem foi enviada. Obrigada!';
+        echo "<meta http-equiv='refresh' content='5;URL=/contact.php'>";
+	} else {
+		echo 'Email nao enviado';
+	}
 
-    //Este sempre deverá existir para garantir a exibição correta dos caracteres
-    $headers  = "MIME-Version: 1.0\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-    $headers .= "From: $name <$email>";
-
-    //Enviar
-    mail($destino, $assunto, $arquivo, $headers);
-
-    echo "<meta http-equiv='refresh' content='10;URL=/contact.php'>";
-
-?>
+} catch (Exception $e) {
+	echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
